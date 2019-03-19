83fb37b91b30d2ca6f2ccb8abc7b0dd3bfde5ea3
DDarrow123 <ddarrowphl@gmail.com>
2019-03-19 17:37:49 -0400

    built in basic responsiveness

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
-// const arrow = document.querySelector(".down-arrow-img");
-console.log("hello!");
-
-// arrow.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+// console.log("hello!");
 
 const arrow = document.querySelector(".down-arrow-img");
 
 arrow.addEventListener("click", e => {
-  // let hashval = arrow.getAttribute(".services-container");
   let target = document.querySelector(".services-listings");
   target.scrollIntoView({
     behavior: "smooth"
   });
-  history.pushState(null, null, hashval);
+
   e.preventDefault();
 });
